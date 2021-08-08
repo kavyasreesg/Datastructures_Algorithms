d6d4dd5d27a325a4f5244624d57927ce2019555b
kavyasreesg <kavyasreesg1997@gmail.com>
2021-08-08 22:35:01 +0530

    udpated string problems

--- Strings/atoi.py
+++ Strings/atoi.py
@@ -3,10 +3,16 @@ class Solution:
     # function should return an integer
     def atoi(self, string):
         # Code here
-        array = list(string)
-        for i in range(len(array)):
-            if i == 0 and (array[0] in ['-', '+'] or array[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']):
+        # array = list(string)
+        # for i in range(len(array)):
+        #     if i == 0 and (array[0] in ['-', '+'] or array[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']):
+        #         pass
+        #     elif array[i] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        #         return -1
+        # return int(string)
+        for i in string:
+            if i in ['-', '+'] or i in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
                 pass
-            elif (array[i] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']):
+            elif i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
                 return -1
         return int(string)
--- Strings/reverse_words_string.py
+++ Strings/reverse_words_string.py
@@ -1,11 +1,17 @@
+"""
+[::-1] has time complexity of O(N) and space complexity of O(1)
+Rather than using loop and using temp variables for swap; using [::-1] is faster
+"""
+
+
 class Solution:
-    
+
     def reverse(self, S):
         list_words = S.split('.')
         return '.'.join(list_words[::-1])
-    
+
+
 if __name__ == '__main__':
-    
     obj = Solution()
     output = obj.reverse("i.like.this.program.very.much")
     print(output)
